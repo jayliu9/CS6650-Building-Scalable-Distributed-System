99a7ff077325a39879b3c9751ca3c651ec4b4cab
Shijie Liu <liushijie@Shijies-MacBook-Pro.local>
2023-10-06 13:06:25 -0700

    reformat

--- assignment1/go-version/go_simple_music_service/go/api_default.go
+++ assignment1/go-version/go_simple_music_service/go/api_default.go
@@ -12,11 +12,11 @@
 package swagger
 
 import (
-	"net/http"
 	"encoding/json"
-    "io/ioutil"
-    "github.com/gorilla/mux"
-    "fmt"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"net/http"
 )
 
 // Global DAO (Data Access Object) instance for Album operations
@@ -24,7 +24,7 @@ var dao *AlbumDAO
 
 // Initializes the DAO for album operations on server start
 func init() {
-    dao = NewAlbumDAO()
+	dao = NewAlbumDAO()
 }
 
 // Handles the HTTP request to retrieve an album's information based on its albumID.
@@ -33,34 +33,33 @@ func init() {
 // Parameters:
 //   - w: The HTTP response writer.
 //   - r: The HTTP request object
-//
 func GetAlbumByKey(w http.ResponseWriter, r *http.Request) {
-    // Extract URL variables from the request.
+	// Extract URL variables from the request.
 	vars := mux.Vars(r)
 
-    // Attempt to retrieve the albumID from the URL parameters.
-    albumId, ok := vars["albumID"]
-    // Check if albumID is missing or empty.
-    if !ok || albumId == ""{
-        // If it is, write an error response and return.
-        writeErrorResponse(w, http.StatusBadRequest, "missing parameters")
-        return
-    }
+	// Attempt to retrieve the albumID from the URL parameters.
+	albumId, ok := vars["albumID"]
+	// Check if albumID is missing or empty.
+	if !ok || albumId == "" {
+		// If it is, write an error response and return.
+		writeErrorResponse(w, http.StatusBadRequest, "missing parameters")
+		return
+	}
 
-    // Fetch album information (currently constant information).
-    albumInfo := dao.GetAlbumByKey(albumId)
-    
-    // Convert the retrieved album information to JSON format.
+	// Fetch album information (currently constant information).
+	albumInfo := dao.GetAlbumByKey(albumId)
+
+	// Convert the retrieved album information to JSON format.
 	respData, err := json.Marshal(albumInfo)
-    // Handle potential JSON marshalling error.
+	// Handle potential JSON marshalling error.
 	if err != nil {
 		writeErrorResponse(w, http.StatusInternalServerError, "Error marshalling JSON")
 		return
 	}
-    // Set response headers and write the JSON response.
+	// Set response headers and write the JSON response.
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
-    w.Write(respData)
+	w.Write(respData)
 }
 
 // Writes a standardized error message in JSON format to the provided response writer.
@@ -84,41 +83,40 @@ func writeErrorResponse(w http.ResponseWriter, statusCode int, message string) {
 // Parameters:
 //   - w: The HTTP response writer to which the result (success or error) will be written.
 //   - r: The HTTP request object
-//
 func NewAlbum(w http.ResponseWriter, r *http.Request) {
-    // Parse the submitted form to extract file parts, considering a max size of 10MB for the form data.
-    err := r.ParseMultipartForm(10 << 20) 
-    if err != nil {
-        http.Error(w, "Unable to parse form", http.StatusBadRequest)
-        return
-    }
+	// Parse the submitted form to extract file parts, considering a max size of 10MB for the form data.
+	err := r.ParseMultipartForm(10 << 20)
+	if err != nil {
+		http.Error(w, "Unable to parse form", http.StatusBadRequest)
+		return
+	}
 
-    // Retrieve the image part from the form.
-    imagePart, _, err := r.FormFile("image")
-    if err != nil {
-        http.Error(w, "Unable to get image", http.StatusBadRequest)
-        return
-    }
-    // Ensure the imagePart stream gets closed after processing.
-    defer imagePart.Close()
-    // Read the entire content of the image file into memory.
-    imageData, _ := ioutil.ReadAll(imagePart)
+	// Retrieve the image part from the form.
+	imagePart, _, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "Unable to get image", http.StatusBadRequest)
+		return
+	}
+	// Ensure the imagePart stream gets closed after processing.
+	defer imagePart.Close()
+	// Read the entire content of the image file into memory.
+	imageData, _ := ioutil.ReadAll(imagePart)
 
-    // Create a response containing the ID of the new album (currently constant albumID) and the size of the uploaded image.
-    albumPostResponse := map[string]interface{}{
-        "ID":       "newAlbumId",
-        "ImageSize" : fmt.Sprintf("%d", len(imageData)),
-    }
+	// Create a response containing the ID of the new album (currently constant albumID) and the size of the uploaded image.
+	albumPostResponse := map[string]interface{}{
+		"ID":        "newAlbumId",
+		"ImageSize": fmt.Sprintf("%d", len(imageData)),
+	}
 
-    // Convert the response data into JSON format.
-    respData, err := json.Marshal(albumPostResponse)
-    if err != nil {
-        http.Error(w, "Error converting response to JSON", http.StatusInternalServerError)
-        return
-    }
+	// Convert the response data into JSON format.
+	respData, err := json.Marshal(albumPostResponse)
+	if err != nil {
+		http.Error(w, "Error converting response to JSON", http.StatusInternalServerError)
+		return
+	}
 
-    // Set the response headers and write the JSON response to the client.
-    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-    w.WriteHeader(http.StatusOK)
-    w.Write(respData)
+	// Set the response headers and write the JSON response to the client.
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write(respData)
 }
